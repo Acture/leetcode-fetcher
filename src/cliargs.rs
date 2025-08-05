857377571c8d6327a728dbc7988c199ba2ddee9e
acture <acturea@gmail.com>
2025-08-05 23:43:47 +0800

    feat(cli): add CLI support for fetching LeetCode questions
    Integrates `clap` for command-line parsing and adds a new `FetchQuestionList` command. Supports output to a file or stdout. Updated dependencies in `Cargo.toml` to include `clap`, `strum`, and `serde_json`.
    
    BREAKING CHANGE: The application now uses a CLI interface instead of default execution logic.
    
    Signed-off-by: acture <acturea@gmail.com>

---
+++ src/cliargs.rs
@@ -0,0 +1,21 @@
+use std::path::PathBuf;
+use clap::ArgGroup;
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Subcommand, Debug, Clone)]
+
+pub enum Commands {
+	FetchQuestionList(FetchQuestionListArgs),
+}
+
+#[derive(Debug, Parser)]
+pub struct CliArgs {
+	#[command(subcommand)]
+	pub command: Commands,
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct FetchQuestionListArgs {
+	#[arg(short, long, default_value = "None")]
+	pub outpath: Option<PathBuf>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 #![feature(more_qualified_paths)]
+use clap::Parser;
+use std::fs::{self, write};
+use std::io::stdout;
+
 mod lib;
+mod cliargs;
 
 
 #[tokio::main]
 async fn main() {
-	lib::grab_questions().await.expect("Failed to grab questions");
+	let args = cliargs::CliArgs::parse();
+	match args.command {
+		cliargs::Commands::FetchQuestionList(args) => {
+			let questions = lib::grab_questions().await.expect("Failed to grab questions");
+			match args.outpath {
+				Some(outpath) => {
+					let file = fs::File::create_new(outpath).expect("Failed to create file");
+					serde_json::to_writer_pretty(file, &questions).expect("Failed to write to file");
+				}
+				_ => {
+					serde_json::to_writer_pretty(stdout(), &questions).expect("Failed to write to file");
+				}
+			}
+		}
+		_ => unimplemented!()
+	}
 }
